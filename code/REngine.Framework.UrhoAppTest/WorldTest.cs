0663d3c7490977f018c0f9b6651fe276266aa345
Ruben Gomes <rbnpontes@gmail.com>
2021-04-24 13:03:17 -0300

    * Added First Test * Update Compile Path

---
+++ code/REngine.Framework.UrhoAppTest/WorldTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace REngine.Framework.UrhoAppTest
+{
+	[TestClass]
+	public class WorldTest : BaseTest
+	{
+		[TestMethod]
+		public void Test_CreateActor()
+		{
+			using (IWorld world = Root.CreateWorld())
+			{
+				Assert.IsNotNull(world.CreateActor());
+			}
+		}
+	}
+}
