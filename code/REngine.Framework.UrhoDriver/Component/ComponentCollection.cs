9556a82bb5623575d701e83ed65ebd10d5598a66
Ruben Gomes <rbnpontes@gmail.com>
2021-04-26 16:07:48 -0300

    * Added Component Collection * Added Component Actor Handler

---
+++ code/REngine.Framework.UrhoDriver/Component/ComponentCollection.cs
@@ -0,0 +1,52 @@
+﻿using REngine.Framework.Components;
+using REngine.Framework.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REngine.Framework.UrhoDriver.Component
+{
+	internal class ComponentCollection : IComponentCollection
+	{
+		private IDictionary<Type, TypeExtractor> _components = new Dictionary<Type, TypeExtractor>();
+
+		private void ValidateType(Type type)
+		{
+			if (_components.ContainsKey(type))
+				throw new ArgumentException("This component has been already registered!");
+		}
+
+		public IComponentCollection Add<Type>()
+		{
+			return Add(typeof(Type));
+		}
+
+		public IComponentCollection Add<Type, Interface>()
+		{
+			return Add(typeof(Type), typeof(Interface));
+		}
+
+		public IComponentCollection Add(Type type, Type @interface = null)
+		{
+			ValidateType(@interface ?? type);
+			TypeExtractor typeExtractor = new TypeExtractor(type);
+			_components[@interface ?? type] = typeExtractor;
+
+			return this;
+		}
+
+		public IComponentCollection Create<T>(IActor actor)
+		{
+			return Create(actor, typeof(T));
+		}
+
+		public IComponentCollection Create(IActor actor, Type type)
+		{
+			actor.CreateComponent(type);
+			return this;
+		}
+
+	}
+}
---
+++ code/REngine.Framework.UrhoDriver/Component/ComponentsActorHandler.cs
@@ -0,0 +1,24 @@
+﻿using REngine.Framework.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REngine.Framework.UrhoDriver.Component
+{
+	internal class ComponentsActorHandler
+	{
+		private Actor _actor;
+		private IComponentCollection _componentCollection;
+		private IDictionary<Type, IList<IComponent>> _components = new Dictionary<Type, IList<IComponent>>();
+
+		public ComponentsActorHandler(Actor actor, IComponentCollection collection)
+		{
+			_componentCollection = collection;
+			_actor = actor;
+		}
+
+
+	}
+}
