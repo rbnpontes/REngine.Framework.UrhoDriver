f8bb0d6e27965fb43dfacd9a4a99cc7490f33de3
Ruben Gomes <rbnpontes@gmail.com>
2021-04-27 15:25:22 -0300

    * Bare Implementation of LightComponent

---
+++ code/REngine.Framework.UrhoDriver/Component/LightComponent.cs
@@ -0,0 +1,24 @@
+﻿using REngine.Framework.Components;
+using REngine.Framework.Mathf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REngine.Framework.UrhoDriver.Component
+{
+	[NativeComponent("Light", typeof(ILight))]
+	internal class LightComponent : NativeComponent, ILight
+	{
+		public LightMode Mode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public Color Color { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public float Intensity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public float Distance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public float Angle { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public float Power { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public bool CastShadow { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		public LightComponent() : base(nameof(LightComponent)) { }
+	}
+}
